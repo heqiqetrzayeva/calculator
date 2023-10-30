740ce072e91d51d9e2403907584f1512f73044e3
heqiqetrzayeva <138764321+heqiqetrzayeva@users.noreply.github.com>
2023-10-30 20:09:29 +0400

    Update Calculate_model.php

--- application/models/Calculate_model.php
+++ application/models/Calculate_model.php
@@ -1,1 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
+class Calculate_model extends CI_Model {
+
+    public function InsertNum($data)
+    {
+        return $this->db->insert('calculator',$data);
+    }
+}
